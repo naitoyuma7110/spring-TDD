3fd1de555862e0f9fe93daf11720d7c615861131
naitoyuma7110 <naitoyuma1118@gmail.com>
2023-04-18 17:03:22 +0900

    数字ペアをスタティックなHashfieldに登録

--- RomanConverter/src/main/java/rs/com/tm/siriusxi/tdd/roman/RomanConverter.java
+++ RomanConverter/src/main/java/rs/com/tm/siriusxi/tdd/roman/RomanConverter.java
@@ -1,11 +1,23 @@
 package rs.com.tm.siriusxi.tdd.roman;
 
+
+import java.util.Hashtable;
+
 /**
  * ローマ数字とアラビア数字の変換を実行するクラス
  *
  * @author naito
  */
 final class RomanConverter {
+//  ローマ数字とアラビア数字のハッシュテーブル(Key:Valueのオブジェクト)を作成し数字ペアを管理
+    private static final Hashtable<Character, Integer> romanSymbols = new Hashtable<Character,Integer>() {
+        {
+            put('I', 1);
+            put('V', 5);
+        }
+    };
+
+
     /**
      * ローマ数字とアラビア数字の変換メソッド
      *
@@ -15,15 +27,12 @@ final class RomanConverter {
     public static int convertRomanToArabicNumber(String roman){
         int sum = 0;
         for(char ch : roman.toCharArray()){
-            if(ch == 'I') {
-                sum += 1;
-            } else if (ch == 'V') {
-                sum +=5;
+            if(romanSymbols.containsKey(ch)){
+                sum += romanSymbols.get(ch);
             }else{
-                throw new IllegalArgumentException(
-                        String.format("Unexpected roman character %s", ch)
-                );
+                throw new IllegalArgumentException(String.format("Unexpected roman character %s", ch));
             }
+
         }
         return sum;
     }
